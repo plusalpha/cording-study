f50050306daa05efec8508043fff9175ff883de5
plusalpha <hyun17171771@gmail.com>
2017-06-26 21:35:51 +0900

    1차 수정

--- 1st_paper3.py
+++ 1st_paper3.py
@@ -12,16 +12,6 @@ game = [Rock, Scissors, Paper]
 
 
 
-# 시간이 조금 더 있었으면 사용자가 가위 바위 보가 아닌 다른 문자를 입력했을 경우도 대응하도록 구현하는 것도 더 고민해봤을 텐데 우선 제출합니다.
-
-# def user_chocie():
-#     user = input("{}, {}, {} 중 하나 내기 : ".format(Rock,Scissors,Paper))
-#     if user != "가위" or user != "바위" or user != "보":
-#         print("가위, 바위, 보 중 하나만 선택하세요")
-#     return user_chocie()
-#
-# user_c = user_chocie()
-
 
 
 
@@ -29,35 +19,151 @@ u_win = 0
 c_win = 0
 
 while u_win < 3 and c_win < 3:
+
     user_c = input("{}, {}, {} 중 하나 내기 : ".format(Rock,Scissors,Paper))
     com_c = random.choice(game)
 
     if user_c == com_c:
         user_c
         print("컴퓨터 : " + com_c)
-        u_win = u_win
-        c_win = c_win
+        u_win = c_win
+        u_win = c_win
         print("현재 스코어{}:{}".format(u_win, c_win))
-    elif user_c == Scissors and com_c == Paper:
-        user_c
-        print("컴퓨터 : " + com_c)
-        u_win = u_win + 1
-        print("현재 스코어{}:{}".format(u_win, c_win))
-    elif user_c == Paper and com_c == Rock:
-        user_c
-        print("컴퓨터 : " + com_c)
-        u_win = u_win + 1
-        print("현재 스코어{}:{}".format(u_win, c_win))
-    elif user_c == Rock and com_c == Scissors:
-        user_c
-        print("컴퓨터 : " + com_c)
-        u_win = u_win + 1
-        print("현재 스코어{}:{}".format(u_win, c_win))
+    elif user_c == Scissors:
+        if com_c == Paper:
+            user_c
+            print("컴퓨터 : " + com_c)
+            c_win = c_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
+        elif com_c == Rock:
+            user_c
+            print("컴퓨터 : " + com_c)
+            c_win = c_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
+    elif user_c == Paper:
+        if com_c == Rock:
+            user_c
+            print("컴퓨터 : " + com_c)
+            c_win = c_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
+        elif com_c == Scissors:
+            user_c
+            print("컴퓨터 : " + com_c)
+            c_win = c_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
+    elif user_c == Rock:
+        if com_c == Scissors:
+            user_c
+            print("컴퓨터 : " + com_c)
+            u_win = u_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
+        elif com_c == Paper:
+            user_c
+            print("컴퓨터 : " + com_c)
+            c_win = c_win + 1
+            print("현재 스코어{}:{}".format(u_win, c_win))
     else:
-        user_c
-        print("컴퓨터 : " + com_c)
-        c_win = c_win + 1
-        print("현재 스코어{}:{}".format(u_win,c_win))
+        print("가위 바위 보 중 하나를 정확히 입력해주세요.")
+
+
+
+
+
+
+#중복 코드 분리 실패
+
+
+# class WhoWin():
+#     u_win = 0
+#     c_win = 0
+#     user_c = input("{}, {}, {} 중 하나 내기 : ".format(Rock,Scissors,Paper))
+#     com_c = random.choice(game)
+#     def user_win(self):
+#         self.user_c
+#         print("컴퓨터 : " + com_c)
+#         self.u_win = self.u_win + 1
+#         print("현재 스코어{}:{}".format(u_win, self.c_win))
+#     def com_win(self):
+#         self.user_c
+#         print("컴퓨터 : " + self.com_c)
+#         self.c_win = self.c_win + 1
+#         print("현재 스코어{}:{}".format(self.u_win, self.c_win))
+#
+# who = WhoWin()
+#
+#
+#
+#
+# who.u_win
+# who.c_win
+#
+# while who.u_win < 3 and who.c_win < 3:
+#
+#     who.user_c
+#     who.com_c
+#
+#     if who.user_c == who.com_c:
+#         who.user_c
+#         print("컴퓨터 : " + who.com_c)
+#         who.u_win = who.c_win
+#         who.u_win = who.c_win
+#         print("현재 스코어{}:{}".format(who.u_win, who.c_win))
+#     elif who.user_c == Scissors:
+#         if who.com_c == Paper:
+#             who.user_win
+#         elif who.com_c == Rork:
+#             who_com_win
+#     elif who.user_c == Paper:
+#         if who.com_c == Rock:
+#             who.user_win
+#         elif who.com_c == Scissors:
+#             who_com_win
+#     elif who.user_c == Rock:
+#         if who.com_c == Scissors:
+#             who.user_win
+#         elif who.com_c == Paper:
+#             who.com_win
+#     else:
+#         print("가위 바위 보 중 하나를 정확히 입력해주세요.")
+
+
+
+#가위 바위 보가 아닌 다른 문자를 입력했을 경우도 대응하도록 구현 X
+
+
+# u_win = 0
+# c_win = 0
+# while u_win < 3 and c_win < 3:
+#
+#     com_c = random.choice(game)
+#     user_c = input("{}, {}, {} 중 하나 내기 : ".format(Rock,Scissors,Paper))
+#
+#     if user_c == com_c:
+#         user_c
+#         print("컴퓨터 : " + com_c)
+#         u_win = u_win
+#         c_win = c_win
+#         print("현재 스코어{}:{}".format(u_win, c_win))
+#     elif user_c == Scissors and com_c == Paper:
+#         user_c
+#         print("컴퓨터 : " + com_c)
+#         u_win = u_win + 1
+#         print("현재 스코어{}:{}".format(u_win, c_win))
+#     elif user_c == Paper and com_c == Rock:
+#         user_c
+#         print("컴퓨터 : " + com_c)
+#         u_win = u_win + 1
+#         print("현재 스코어{}:{}".format(u_win, c_win))
+#     elif user_c == Rock and com_c == Scissors:
+#         user_c
+#         print("컴퓨터 : " + com_c)
+#         u_win = u_win + 1
+#         print("현재 스코어{}:{}".format(u_win, c_win))
+#     else:
+#         user_c
+#         print("컴퓨터 : " + com_c)
+#         c_win = c_win + 1
+#         print("현재 스코어{}:{}".format(u_win,c_win))
 
 
 
